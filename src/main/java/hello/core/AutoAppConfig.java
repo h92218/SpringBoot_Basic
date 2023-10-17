ff0a63c63721b933ad0abcebf68751a175acf131
현선 박 <hspark@chonf.co.kr>
2023-10-17 16:38:21 +0900

    컴포넌트 스캔과 의존관계 자동 주입

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION,classes = Configuration.class)
+)
+//@Component가 붙은 클래스를 스캔해서 스프링 빈으로 등록
+//@Bean으로 등록해준 클래스 없음
+public class AutoAppConfig {
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,18 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("memberService2")
+//이름 직접 붙이기 가능
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
+    //Component 어노테이션사용시 사용
+    //의존관계 자동 주입
+    //기본적으로는 타입이 같은 빈을 찾아서 주입한다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }

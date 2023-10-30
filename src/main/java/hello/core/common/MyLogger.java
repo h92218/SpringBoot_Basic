0a52e3d98cc589db2e41f0437a2549e7bbdca1c1
현선 박 <sh717510@gmail.com>
2023-10-30 17:13:45 +0900

    ObjectProvider 대신 proxyMode 사용하기

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value="request")
+@Scope(value="request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+//가짜 프록시 클래스를 만들어두고 다른 빈에 미리 주입해 둘 수 있다.
 public class MyLogger {
 
     private String uuid;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
     private final MyLogger myLogger;
     //mylogger의 스코프 : request 가 들어오고 나갈때 까지
     /* mylogger 의존관계 주입 시 scope가 request인데
       spring 시작 시 http request가 없어서 request 스코프를 가진 MyLogger 빈은 아직 생성이 안 됨
       => Scope 'request' is not active for the current thread;
+      => ObjectProvider 로 스프링 컨테이너에 빈의 호출 시점을 지연하여 해결함
      */
 
 
@@ -25,6 +29,10 @@ public class LogDemoController {
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString();
+        //MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        //myLogger = class hello.core.common.MyLogger$$EnhancerBySpringCGLIB$$f4a6c885 <<- 가짜
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
